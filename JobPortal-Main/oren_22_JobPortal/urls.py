d396230e46d3b1800d286226f1591ba56f68d09d
Oren M Dessai <orendessai@gmail.com>
2026-08-18 19:57:26 +0300

    Update urls.py

--- JobPortal-Main/oren_22_JobPortal/urls.py
+++ JobPortal-Main/oren_22_JobPortal/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('JobPortalApp.urls'))
     
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
